35fe55dbe4d052554b493eb36bcec8819fa19836
followhappyq <followhappyq@gmail.com>
2018-09-27 15:19:48 +0300

    works

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,8 @@ const game = {
   blocks: [],
   rows: 4,
   cols: 10,
+  score: 0,
+  running: true,
   sprites: {
     background: undefined,
     platform: undefined,
@@ -21,6 +23,9 @@ const game = {
       }
       if (e.keyCode == 39) {
         game.platform.dx = game.platform.velocity;
+      }
+      if (e.keyCode == 32) {
+        game.platform.releaseBall();
       }
     });
     window.addEventListener("keyup", e => {
@@ -42,7 +47,8 @@ const game = {
           x: 68 * col + 63,
           y: 38 * row + 50,
           width: 64,
-          height: 32
+          height: 32,
+          isAlive: true
         });
       }
     }
@@ -63,35 +69,119 @@ const game = {
     this.ctx.drawImage(this.sprites.ball, this.ball.x, this.ball.y);
 
     this.blocks.forEach(function(item) {
-      this.ctx.drawImage(this.sprites.blocks, item.x, item.y);
+      if (item.isAlive) {
+        this.ctx.drawImage(this.sprites.blocks, item.x, item.y);
+      }
     }, this);
   },
   update: function() {
+    if (this.ball.collide(this.platform)) {
+      this.ball.bumpPlatform(this.platform);
+    }
+
     if (this.platform.dx) {
       this.platform.move();
     }
+
+    if (this.ball.dx || this.ball.dy) {
+      this.ball.move();
+    }
+
+    this.blocks.forEach(function(item) {
+      if (item.isAlive) {
+        if (this.ball.collide(item)) {
+          this.ball.bumpBlock(item);
+        }
+      }
+    }, this);
+
+    this.ball.checkingBounds();
   },
   run: function() {
     this.update();
     this.render();
-
-    window.requestAnimationFrame(() => {
-      game.run();
-    });
+    if (this.running) {
+      window.requestAnimationFrame(() => {
+        game.run();
+      });
+    }
+  },
+  over: function(message) {
+    this.running = false;
+    console.log(message);
   }
 };
 
 game.ball = {
-  width: 22,
-  height: 22,
-  frame: 0,
+  width: 20,
+  height: 20,
   x: 390,
-  y: 480
+  y: 478,
+  dx: 0,
+  dy: 0,
+  velocity: 3,
+  jump: function() {
+    this.dy = -this.velocity;
+    this.dx = -this.velocity;
+  },
+  move: function() {
+    this.x += this.dx;
+    this.y += this.dy;
+  },
+  collide: function(item) {
+    let x = this.x + this.dx;
+    let y = this.y + this.dy;
+
+    if (
+      x + this.width > item.x &&
+      x < item.x + item.width &&
+      y + this.height > item.y &&
+      y < item.y + item.height
+    ) {
+      return true;
+    }
+    return false;
+  },
+  bumpBlock: function(block) {
+    this.dy *= -1;
+    block.isAlive = false;
+    ++game.score;
+
+    if (game.score >= game.blocks.length) {
+      game.over("You win!");
+    }
+  },
+  onTheLeftSide: function(platform) {
+    return this.x + this.width / 2 < (platform.x + platform.width) / 2;
+  },
+  bumpPlatform: function(platform) {
+    this.dy = -this.velocity;
+    this.dx = this.onTheLeftSide(platform) ? -this.velocity : this.velocity;
+  },
+  checkingBounds: function() {
+    let x = this.x + this.dx;
+    let y = this.y + this.dy;
+
+    if (x < 0) {
+      this.x = 0;
+      this.dx = this.velocity;
+    } else if (x + this.width > game.width) {
+      this.x = game.width - this.width;
+      this.dx = -this.velocity;
+    } else if (y < 0) {
+      this.y = 0;
+      this.dy = this.velocity;
+    } else if (y + this.height > game.height) {
+      game.over("You lose!");
+    }
+  }
 };
 
 game.platform = {
   x: 350,
   y: 500,
+  width: 104,
+  height: 24,
   velocity: 7,
   dx: 0,
   ball: game.ball,
@@ -107,6 +197,12 @@ game.platform = {
 
     if (this.ball) {
       this.ball.dx = 0;
+    }
+  },
+  releaseBall: function() {
+    if (this.ball) {
+      this.ball.jump();
+      this.ball = false;
     }
   }
 };
